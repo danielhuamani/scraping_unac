81423475beb27a54a951bc9ed41f2b056b6fe2b8
haro28 <harol.calzada@gmail.com>
2017-07-06 02:48:31 -0500

    Agregando el view para extraer el csv

--- scraping_unac/unac/urls.py
+++ scraping_unac/unac/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from .views import (cargar_data, index, alumnos, notas)
+from .views import (cargar_data, index, alumnos, notas, generate_csv_data_format_1)
 
 urlpatterns = [
     url(r"^cargar_data/$", cargar_data, name="cargar_data"),
     url(r"^$", index, name="index"),
     url(r"^alumnos/$", alumnos, name="alumnos"),
     url(r"^alumnos/(?P<pk>\d+)/$", notas, name="notas"),
- ]
+    url(r"^generate_csv_data_format_1/$", generate_csv_data_format_1, name="generate_csv_data_format_1"),
+
+]
