12f4fffad61b8612cad7376fe9fde01b78a20615
haro28 <harol.calzada@gmail.com>
2017-07-07 22:21:10 -0500

    Cambiando el tipo de bd sqlite to postgres

--- scraping_unac/scraping_unac/settings.py
+++ scraping_unac/scraping_unac/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'scraping_unac.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scraping_unac',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
