fce7da31c7e5a81d9cbef03e0db8e16a8a5e6262
haro28 <harol.calzada@gmail.com>
2017-07-08 20:51:03 -0500

    Agregando mas features to dataset

--- scraping_unac/unac/views.py
+++ scraping_unac/unac/views.py
@@ -79,6 +79,21 @@ def generate_csv_data_format_1(request):
                         result_final[nota.alumno_id][nota.curso.nombre] = 0
                     else:
                         result_final[nota.alumno_id][nota.curso.nombre] = nota.nota
+                # ASGINAMOS EL RENDIMIENTO ACADEMICO
+                if len(result_final[nota.alumno_id].keys()) == cantidad_cursos + 1:
+                    print ('PSE EL IF')
+                    total_notas = 0
+                    for key in result_final[nota.alumno_id].keys():
+                        if key != 'CODIGO ALUMNO':
+                            total_notas += int(result_final[nota.alumno_id][key])
+                    promedio = float(total_notas) / 55
+                    result_final[nota.alumno_id]['PROMEDIO'] = promedio
+                    if promedio >= 13:
+                        result_final[nota.alumno_id]['RENDIMIENTO'] = 'A'
+                    elif 11 <= promedio < 13:
+                        result_final[nota.alumno_id]['RENDIMIENTO'] = 'B'
+                    else:
+                        result_final[nota.alumno_id]['RENDIMIENTO'] = 'C'
             else:
                 result_final[nota.alumno_id] = {}
                 result_final[nota.alumno_id]['CODIGO ALUMNO'] = nota.alumno.codigo
@@ -87,11 +102,11 @@ def generate_csv_data_format_1(request):
                 else:
                     result_final[nota.alumno_id][nota.curso.nombre] = nota.nota
 
-    ## Creo todas las cabeceras de mi csv
+    # # Creo todas las cabeceras de mi csv
     cabeceras = set(cursos_curricula_antigua)
     for key in result_final.keys():
         cabeceras.update(result_final[key].keys())
-    ## Genero el CSV
+    # # Genero el CSV
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="data_format_1.csv"'
     writer = csv.DictWriter(response, cabeceras)
